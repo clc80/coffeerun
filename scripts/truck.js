b33d48186d697139fa3a9af92f1b9111b4bb2896
clc80 <clc_80@yahoo.com>
2017-11-22 13:45:23 -0800

    added functions that allows the user to add and delete orders

--- scripts/truck.js
+++ scripts/truck.js
@@ -6,6 +6,19 @@
     this.truckId = truckId;
     this.db = db;
   }
+
+  //Store a coffee order
+  Truck.prototype.createOrder = function (order) {
+    console.log("Adding order for " + order.emailAddress);
+    this.db.add(order.emailAddress, order);
+  };
+
+  //Remove orders
+  Truck.prototype.deliverOrder = function (customerId) {
+    console.log("Delivering order for " + customerId);
+    this.db.remove(customerId);
+  };
+  
     App.Truck = Truck;
     window.App = App;
 
