9deb4c8522adc8d1363eeb0422b72f358a4132e9
clc80 <clc_80@yahoo.com>
2017-11-22 11:06:56 -0800

    Working with DataStore using only its prototype methods

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -1,27 +1,30 @@
-(function (window){
+(function (window) {
   'use strict';
   var App = window.App || {};
 
   function DataStore() {
     this.data = {};
   }
-
+  //adds new data to the database
   DataStore.prototype.add = function (key, val) {
     this.data[key] = val;
   };
 
-  DataStore.prototype.get = function (key) {
+  //Accesses the database
+  //This one is one item at a time by key
+  DataStore.prototype.get = function(key) {
     return this.data[key];
   };
-
+  //This one does it all at once
   DataStore.prototype.getAll = function() {
     return this.data;
   };
 
-  DataStore.prototype.remove = function(key) {
+  //To remove data
+  DataStore.prototype.remove = function (key) {
     delete this.data[key];
   };
 
   App.DataStore = DataStore;
   window.App = App;
-}) (window);
+})(window);
