20c04158ce3860a6c87bfaec5475e725b8a01de6
clc80 <clc_80@yahoo.com>
2017-11-26 15:01:17 -0800

    fixed bug that needed the bind method

--- scripts/truck.js
+++ scripts/truck.js
@@ -26,7 +26,7 @@
     console.log("Truck #" + this.truckId + " has pending orders: ");
     customerIdArray.forEach(function (id) {
       console.log(this.db.get(id));
-    });
+    } .bind(this));
   };
     App.Truck = Truck;
     window.App = App;
