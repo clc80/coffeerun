9c30f1dbe64a1eed5df2dfc9abc0d2f1e29e62ed
clc80 <clc_80@yahoo.com>
2017-11-26 17:06:34 -0800

    cleaned up code and removed bugs

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,10 @@
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+
+  //create a new instance of truck
+  var myTruck = new Truck('ncc-1701', new DataStore());
+  //export the variable to a global variable
+  window.myTruck = myTruck;
+  
 })(window);
